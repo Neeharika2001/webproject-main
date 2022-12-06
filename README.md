49509e4d367122e8d946369e3fa0cab2d27be89e
Neeharika2001 <damacharlaneeharika@gmail.com>
2022-12-06 04:18:45 -0500

    WEB

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1> It is a NOTE Application created by Neeharika</h1>
+<p> This application has NOTE, Register and Login pages where the user can create and use notes</p>
+<br>
+<img src="https://github.com/Neeharika2001/Web_Project_1/blob/main/public/images/ER.png" width="600"/>
---
+++ server/models/note.js
@@ -0,0 +1,63 @@
+const con = require("./db_connect");
+
+
+async function createTable() {
+let sql=`CREATE TABLE IF NOT EXISTS notes (
+  noteID INT NOT NULL AUTO_INCREMENT,
+  emailid VARCHAR(255) NOT NULL,
+  notes VARCHAR(255) NOT NULL,
+  CONSTRAINT notePK PRIMARY KEY(noteID)
+); `
+await con.query(sql);
+}
+createTable();
+
+async function create(note) {
+
+const sql = `INSERT INTO notes (emailid, notes)
+  VALUES ("${note.emailid}","${note.notes}");
+`
+
+await con.query(sql);
+return {success:"Note Added"};
+}
+
+
+async function getAllNotes() {
+ const sql = "SELECT * FROM notes;";
+ let notes = await con.query(sql);
+ console.log(notes)
+ return notes;
+}
+
+
+async function getNote(note) {
+  let sql;
+  
+    sql = `
+      SELECT * FROM notes
+       WHERE noteID = ${note.noteID}
+    `
+  
+  return await con.query(sql);  
+}
+async function deleteNote(note) {
+    let sql = `DELETE FROM notes
+      WHERE noteID = ${note.noteID}
+    `
+    await con.query(sql);
+    }
+async function editNote(note) {
+  let sql = `UPDATE notes 
+    SET notes = "${note.notes}"
+    WHERE noteID = ${note.noteID}
+  `;
+  
+  await con.query(sql);
+  let updatedNote = await getNote(note);
+  return updatedNote[0];
+  }
+
+
+
+module.exports = { getAllNotes, getNote, create, deleteNote, editNote};
